b411cf669befe9c29af2d9423a2af4bac5f27c04
Marc Weber <webermarcolivier@gmail.com>
2018-05-12 13:52:24 +0200

    minor modifications.

--- src/pipeline_roesti_sort_CDS_BED.sh
+++ src/pipeline_roesti_sort_CDS_BED.sh
@@ -10,15 +10,15 @@ GENOME_CDS_BED_FILE=$1
 echo ${GENOME_CDS_BED_FILE}
 NTHREADS=1
 
-mv ${GENOME_CDS_BED_FILE} ${GENOME_CDS_BED_FILE}.unsorted
-while [ ! -f ${GENOME_CDS_BED_FILE}.unsorted ]
-do
-  sleep 2
-done
+# mv ${GENOME_CDS_BED_FILE} ${GENOME_CDS_BED_FILE}.unsorted
+# while [ ! -f ${GENOME_CDS_BED_FILE}.unsorted ]
+# do
+#   sleep 2
+# done
 
-/users/lserrano/mweber/local/bin/sort ${GENOME_CDS_BED_FILE}.unsorted -k 1,1 -k 2,2n -k 3,3n > ${GENOME_CDS_BED_FILE}
+# /users/lserrano/mweber/local/bin/sort ${GENOME_CDS_BED_FILE}.unsorted -k 1,1 -k 2,2n -k 3,3n > ${GENOME_CDS_BED_FILE}
 
-while [ ! -f ${GENOME_CDS_BED_FILE} ]
-do
-  sleep 2
-done
+# while [ ! -f ${GENOME_CDS_BED_FILE} ]
+# do
+#   sleep 2
+# done
